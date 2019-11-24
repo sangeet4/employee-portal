0bf722db5596fece55c1585338ffa1054c548991
sangeet4 <and.sangeet@gmail.com>
2019-11-24 17:55:41 +0530

    Implementation and database connection cred added

--- employee-portal-service/src/main/java/com/example/service/EmployeeServiceImpl.java
+++ employee-portal-service/src/main/java/com/example/service/EmployeeServiceImpl.java
@@ -2,17 +2,27 @@ package com.example.service;
 
 import com.example.domain.Employee;
 import com.example.exception.InvalidEmployeeException;
+import com.example.repository.EmployeeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDao employeeDao) { this.employeeDao = employeeDao; }
+
     @Override
     public List<Employee> getAllEmployees(){
-        return null;
+        return (List<Employee>) employeeDao.findAll();
     }
 
     @Override
     public Employee saveEmployee(Employee employee) throws InvalidEmployeeException {
-        return null;
+        return employeeDao.save(employee);
     }
 }
