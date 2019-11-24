46582f03998fc6c5e1ceb994a034c8f0faac12e7
sangeet4 <and.sangeet@gmail.com>
2019-11-24 20:16:46 +0530

    logging added

--- employee-portal-service/src/main/java/com/example/EmployeePortalApplication.java
+++ employee-portal-service/src/main/java/com/example/EmployeePortalApplication.java
@@ -1,13 +1,17 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeePortalApplication {
+	private static final Logger LOGGER1 = LoggerFactory.getLogger(EmployeePortalApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePortalApplication.class, args);
+		LOGGER1.info("\n\n*************************** EMPLOYEE PORTAL SERVICE APPLICATION STARTED ***************************\n");
 	}
 
 }
