fd159b28b343a55e9ea7d103f598a97fdeb568d4
sangeet4 <and.sangeet@gmail.com>
2019-11-24 17:55:41 +0530

    Implementation and database connection cred added

--- employee-portal-service/src/main/java/com/example/controller/EmployeeController.java
+++ employee-portal-service/src/main/java/com/example/controller/EmployeeController.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 
 import com.example.domain.Employee;
+import com.example.exception.InvalidEmployeeException;
 import com.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -19,11 +24,21 @@ public class EmployeeController {
 
     @GetMapping(value = "all")
     public ResponseEntity<?> getAllEmployees(){
-        return null;
+        try{
+            return new ResponseEntity<List<Employee>>(employeeService.getAllEmployees(), HttpStatus.OK);
+        } catch(Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping(value = "add")
-    public ResponseEntity<?> saveEmployee(Employee employee){
-        return null;
+    public ResponseEntity<?> saveEmployee(@Valid @RequestBody Employee employee){
+        try{
+            return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.OK);
+        } catch(InvalidEmployeeException e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch(Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
