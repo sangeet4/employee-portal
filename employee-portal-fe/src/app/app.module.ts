c54aab62d80a02accaab6ecfb19e381a048882d9
sangeet4 <and.sangeet@gmail.com>
2019-11-25 05:32:46 +0530

    register employee added

--- employee-portal-fe/src/app/app.module.ts
+++ employee-portal-fe/src/app/app.module.ts
@@ -2,22 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 import { EmployeeService } from './employee.service';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ViewEmployeeComponent
+    ViewEmployeeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgbDatepickerModule,
+    FormsModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
